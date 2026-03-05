bc0de00a9609d53967f313cf32b576b8538acbca
SimIceDev <simonicedev@gmail.com>
2026-03-05 17:05:22 +0100

    small cleanup

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,7 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-export function cn(...inputs: ClassValue[]) {
+// Need this for my shadcn setup to work with the className prop in the components. It merges the class names and handles conflicts properly.
+export function mergeClassNames(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
